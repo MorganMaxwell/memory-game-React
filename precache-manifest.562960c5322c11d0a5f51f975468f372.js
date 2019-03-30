b6216b1943d15da92efc773efc00e69c25d1f758
Morgan M <morgan.maxwell20@gmail.com>
2019-03-30 12:29:39 -0600

    Updates

---
+++ precache-manifest.562960c5322c11d0a5f51f975468f372.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "75ea70d79bd65cda73b77d01b7810c3c",
+    "url": "/MorganMaxwell/memory-game-React/static/media/shaggy.75ea70d7.png"
+  },
+  {
+    "revision": "345950e80bf6f9d71a5f6b9fcd03d0b3",
+    "url": "/MorganMaxwell/memory-game-React/static/media/scooby-doo.345950e8.png"
+  },
+  {
+    "revision": "d440af5789d2a1767975107dee93929c",
+    "url": "/MorganMaxwell/memory-game-React/static/media/daphne.d440af57.png"
+  },
+  {
+    "revision": "93feb8db4daa8fcc7446",
+    "url": "/MorganMaxwell/memory-game-React/static/js/runtime~main.ea13a993.js"
+  },
+  {
+    "revision": "b64bdd169527512d83ef",
+    "url": "/MorganMaxwell/memory-game-React/static/js/main.935c8f7b.chunk.js"
+  },
+  {
+    "revision": "1601b01c9cbe81f35c0e",
+    "url": "/MorganMaxwell/memory-game-React/static/js/2.32dd646d.chunk.js"
+  },
+  {
+    "revision": "b64bdd169527512d83ef",
+    "url": "/MorganMaxwell/memory-game-React/static/css/main.0d77f551.chunk.css"
+  },
+  {
+    "revision": "7f71e84f02fb2f5679d856898e0118b7",
+    "url": "/MorganMaxwell/memory-game-React/index.html"
+  }
+];
